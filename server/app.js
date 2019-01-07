eea0b3c70ee606b03481b6a2ed2b32d2f2528efc
Quentin Carlin <quentincarlin@Quentins-MacBook-Air.local>
2019-01-07 14:55:51 -0500

    added user functions

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
-// require('dotenv').config();
+require('dotenv').config();
 
 let express = require('express');
 let app = express();
 const sequelize = require('./db')
 const user = require('./controllers/usercontroller')
+const bodyParser = require('body-parser')
 
 
 sequelize.sync();
+
+app.use(bodyParser.json());
+
+app.use(require('./middleware/header'))
 
 app.use('/user', user)
 
--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs')
 const User = require('../db').import('../models/user')
 const validatesession = require('../middleware/validate-session')
 
+
 router.post('/signup', (req, res) => {
     User.create({
         firstName: req.body.firstName,
@@ -14,25 +15,72 @@ router.post('/signup', (req, res) => {
     })
     .then(
         createSuccess = (user) => {
-            let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24})
+            let token = jwt.sign({id: params.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24})
             console.log(token)
 
             res.json({
-                user: User({
-                    firstName: User.firstName,
-                    lastName: User.lastName,
-                    userName: User.userName,
-                    email: User.email
-                }),
+                user: user,
                 message: 'user created',
                 sessionToken: token
             })
         },
-        creatError = err => res.send(500, err.message)
+        createError = err => res.send(500, err.message)
+    )
+})
+router.post('/signin', (req, res) => {
+    User.findOne({ where: {email: req.body.email } } )
+    .then(
+        user => {
+            if(user) {
+                bcrypt.compare(req.body.password, user.password, (err, matches) => {
+                    if (matches) {
+                        let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24})
+                        res.json({
+                            user: user,
+                            message: 'It worked!!!',
+                            sessionToken: token
+                        })
+                    } else {
+                        res.status(502).send({error: 'bad gateway'})
+                    }
+                })
+            } else {
+                res.status(500).send({error: 'failed to authenticate'})
+            }
+        },
+        err => res.status(501).send({error: 'failed to process'})
     )
 })
 
+router.get('/findall', validatesession, (req, res) => {
+    User.findAll()
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({error: err}))
+})
 
+router.get('/findone', validatesession, (req, res) => {
+    User.findOne({where: {id: req.user.id} } )
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({error: err}))
+})
 
+router.delete('/', validatesession, (req, res) => {
+    if(!req.errors) {
+        User.destroy({where: {id: req.useron.id} } )
+        .then(user => res.status (200).json(user))
+        .catch(err => res.json(req.error))
+    } else {
+        res.status(500).json(req.error)
+    }
+})
+router.put('/update/:id', validatesession, (req, res) => {
+    if(!req.errors) {
+        User.update(req.body, {where: {id: req.user.id} } )
+        .then(user => res.status (200).json(user))
+        .catch(err => res.json(req.error))
+    } else {
+        res.status(500).json(req.error)
+    }
+})
 
 module.exports = router;
